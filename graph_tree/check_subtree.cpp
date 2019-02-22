a84ffef69142ea625296b3b98e5b9540726cc5d6
Lizzy Presland <lizzy.presland@gmail.com>
2019-02-22 12:37:35 -0800

    added another test for identical subtrees

--- graph_tree/check_subtree.cpp
+++ graph_tree/check_subtree.cpp
@@ -30,7 +30,17 @@ void test2() {
     assert(!success);
 }
 
+void test3() {
+    string inOrder[15]{"h", "h", "q", "c", "j", "o", "s", "a", "e", "i", "k", "n", "p", "r", "t"};
+    BinTree<string> b1(inOrder, 15);
+    string subtree[7]{"h", "c", "j", "a", "e", "i", "k"};
+    BinTree<string> b2(subtree, 7);
+    bool success = b1.isSubtree(b2);
+    assert(success);
+}
+
 int main() {
     test1();
     test2();
+    test3();
 }
