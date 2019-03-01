83f781a24a2da5ba48104348a82bc4f36d993979
Lizzy Presland <lizzy.presland@gmail.com>
2019-02-28 19:51:31 -0800

    implemented dynamic solution for coins problem

--- maze_dynprog/coins/coins.py
+++ maze_dynprog/coins/coins.py
@@ -17,33 +17,24 @@ def coins_naive(amount, denominations, denom_index):
     # return the permuatation count for this subproblem
     return permutations
 
-def main():
-    # initialize denominations array
-    denoms = [25, 10, 5, 1]
-    print("Testing naive solution:")
-    # for 5 cents, we expect the result to be 2
-    print("Input: 5\tExpected: 2\tActual: " + str(coins_naive(5, denoms, 0)) + "\tCorrect? %s" % str(coins_naive(5, denoms, 0) == 2))
-    # for 10 cents, we expect the result to be 4
-    print("Input: 10\tExpected: 4\tActual: " + str(coins_naive(10, denoms, 0)) + "\tCorrect? %s" % str(coins_naive(10, denoms, 0) == 4))
-    '''
-        For 25 cents, we expect 13 combinations:
-        | P  | N  | D  | Q  |
-        |----|----|----|----|
-        | 25 | 0  | 0  | 0  |
-        | 20 | 1  | 0  | 0  |
-        | 15 | 2  | 0  | 0  |
-        | 10 | 3  | 0  | 0  |
-        | 5  | 4  | 0  | 0  |
-        | 0  | 5  | 0  | 0  |
-        | 15 | 0  | 1  | 0  |
-        | 10 | 1  | 1  | 0  |
-        | 5  | 2  | 1  | 0  |
-        | 0  | 3  | 1  | 0  |
-        | 5  | 0  | 2  | 0  |
-        | 0  | 1  | 2  | 0  |
-        | 0  | 0  | 0  | 1  |
-    '''
-    print("Input: 25\tExpected: 13\tActual: " + str(coins_naive(25, denoms, 0)) + "\tCorrect? %s" % str(coins_naive(25, denoms, 0) == 13))
-    return
+def coins_dynamic(amount, denoms, denom_index, previously_computed_values):
+    # base case: we have processed all possible larger coin denominations 
+    # for this subproblem, and are only inspecting pennies now
+    if denom_index >= len(denoms) - 1:
+        return 1
+    # base case
+    # If the recursive subproblem has already been solved,
+    # use the existing value! 
+    # NOTE: This is the core component of the dynamic implementation.
+    if previously_computed_values[amount][denom_index] > 0:
+        return previously_computed_values[amount][denom_index]
+    # create local variable to store number of permutations for subproblem
+    permutations = 0
+    # now, incrementally create subproblems for the current denomination
+    for i in range(0, (amount // denoms[denom_index]) + 1):
+        # compute permutations for subproblems of smaller denominations
+        # (Recursive call)
+        permutations += coins_dynamic(amount - (denoms[denom_index] * i), denoms, denom_index + 1, previously_computed_values)
+    previously_computed_values[amount][denom_index] = permutations
+    return permutations
 
-main()
