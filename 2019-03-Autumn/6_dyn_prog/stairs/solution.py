ac9aae04a56dc125eee20ff42a6609d96b54e4db
etcadinfinitum <lizzy.presland@gmail.com>
2019-11-14 22:42:03 -0800

    Add solution writeup for dynprog Q1 (fall 2019)
    Also made a few small improvements for code readability and simplicity.

--- 2019-03-Autumn/6_dyn_prog/stairs/solution.py
+++ 2019-03-Autumn/6_dyn_prog/stairs/solution.py
@@ -1,31 +1,26 @@
 def step_combos_naive(n: int):
     if n < 0:
-        # we have exceeded our 
+        # we have exceeded the number of steps in the staircase
         return 0
     if n == 0:
-        # we have used up all steps in this recursive path
+        # we have completed a unique stair pattern
         return 1
     return step_combos_naive(n - 1) + \
            step_combos_naive(n - 2) + \
            step_combos_naive(n - 3)
 
-class ListWrapper(object):
-    def __init__(self, n: int):
-        self.combos = [0 for i in range(n)]
-
 def step_combos(n: int):
-    combos = ListWrapper(n)
+    combos = [0 for i in range(n)]
     return combos_helper(n, combos)
 
-def combos_helper(n: int, combos: ListWrapper):
+def combos_helper(n: int, combos: list):
     if n < 0:
         return 0
     if n == 0:
         return 1
-    if combos.combos[n - 1] != 0:
-        return combos.combos[n - 1]
-    val = combos_helper(n - 1, combos) + \
-          combos_helper(n - 2, combos) + \
-          combos_helper(n - 3, combos)
-    combos.combos[n - 1] = val
-    return val
+    if combos[n - 1] == 0:
+        val = combos_helper(n - 1, combos) + \
+              combos_helper(n - 2, combos) + \
+              combos_helper(n - 3, combos)
+        combos[n - 1] = val
+    return combos[n - 1]
