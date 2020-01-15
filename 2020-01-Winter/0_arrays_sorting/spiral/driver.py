09edb428a0b6d1fc65fe9d09f60b575cd4ff3fa0
etcadinfinitum <lizzy.presland@gmail.com>
2020-01-14 23:49:03 -0800

    Add solution code and driver/tests

---
+++ 2020-01-Winter/0_arrays_sorting/spiral/driver.py
@@ -0,0 +1,24 @@
+from solution import *
+
+def print_matrix(mtx):
+    print('[')
+    for row in mtx:
+        print('    ', str(row))
+    print(']')
+
+def main(): 
+    print('Test 4x4 alphabet:')
+    t1 = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], 
+          ['i', 'j', 'k', 'l'], ['m', 'n', 'o', 'p']]
+    print_matrix(t1)
+    print('Result: ', str(spiral_order(t1)))
+    print('\nTest 5x7 ints:')
+    t2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10],
+          [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+          [21, 22, 23, 24, 25], [26, 27, 28, 29, 30],
+          [31, 32, 33, 34, 35]]
+    print_matrix(t2)
+    print('Result: ', str(spiral_order(t2)))
+
+if __name__=='__main__':
+    main()
