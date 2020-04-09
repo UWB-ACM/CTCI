290205cdb7e6cc84f3093e9812696880cb462d8a
huyn8 <huyn8@uw.edu>
2020-04-09 15:29:34 -0700

    driver+solution for question 1

---
+++ 2020-02-Spring/0_arrays_matrix/buy_stock/driver.java
@@ -0,0 +1,29 @@
+public class driver{
+    public static void main(String [] args){
+        int [] arr1 = {3};
+        int [] arr2 = {1,9};
+        int [] arr3 = {8,1};
+        int [] arr4 = {7,1,5,3,6,4};
+        int [] arr5 = {6,1,1,23,4,54,33,4,1,9,2,3,4,5,9,6,4,5,3,100};
+
+        Test("Max profit for arr1 is " + maxProfit(arr1), maxProfit(arr1) == 0);
+        Test("Max profit for arr2 is " + maxProfit(arr2), maxProfit(arr2) == 8);
+        Test("Max profit for arr3 is " + maxProfit(arr3), maxProfit(arr3) == 0);
+        Test("Max profit for arr4 is " + maxProfit(arr4), maxProfit(arr4) == 5);
+        Test("Max profit for arr5 is " + maxProfit(arr5), maxProfit(arr5) == 99);
+
+    }
+
+    //Your solution here
+    public static int maxProfit(int prices[]) {
+        return maxprofit;
+    }
+
+    //For testing
+    public static void Test(String testDescription, boolean testPasses) {
+		if (testPasses)
+			System.out.println("Test PASSED: " + testDescription);
+		else
+			System.out.println("Test FAILED: " + testDescription);
+	}
+}
---
+++ 2020-02-Spring/0_arrays_matrix/buy_stock/solution.java
@@ -0,0 +1,20 @@
+public class solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+//Complexity Analysis
+
+//Time complexity : O(n)O(n). Only a single pass is needed.
+
+//Space complexity : O(1)O(1). Only two variables are used.
+
+}
