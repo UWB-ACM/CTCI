dc4bb0bb5e783d87877c2f236104c59cedb7f580
hollowsunsets <isabelgiang@gmail.com>
2020-04-09 09:49:09 -0700

    Add completed solution

--- 2020-02-Spring/0_arrays_matrix/word_search/java/Solution.java
+++ 2020-02-Spring/0_arrays_matrix/word_search/java/Solution.java
@@ -1,29 +1,34 @@
 class Solution {
-    private static String word;
     
-    public boolean exist(char[][] board, String word) {
-        this.word = word;
+    public static void main(String[] args) {
+        char[][] testBoard = {{ 'A','B','C','E' }, { 'S','F','C','S' }, { 'A','D','E','E' }};
+        System.out.println(exist(testBoard, "ABCCED")); // Should print true
+        System.out.println(exist(testBoard,"SEE"));     // Should print true
+        System.out.println(exist(testBoard, "ABCB"));   // Should print false
+    }
+    public static boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (existsHelper(board, 0, i, j)) {
+                if (backtrack(board, 0, i, j, word)) {
                     return true;
                 }
             }
         }
+        return false;
     }
 
-    private boolean existsHelper(char[][] board, int index, int row, int col) {
+    private static boolean backtrack(char[][] board, int index, int row, int col, String word) {
         if (index == word.length()) {
             return true;
         }
         if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || word.charAt(index) != board[row][col]) {
             return false;
         } else {
-            board[row][col] = "*";
-            boolean result = existsHelper(board, index + 1, row + 1, col) ||
-                             existsHelper(board, index + 1, row, col + 1) ||
-                             existsHelper(board, index + 1, row - 1, col) ||
-                             existsHelper(board, index + 1, row, col - 1);
+            board[row][col] = '*';
+            boolean result = backtrack(board, index + 1, row + 1, col, word) ||
+                             backtrack(board, index + 1, row, col + 1, word) ||
+                             backtrack(board, index + 1, row - 1, col, word) ||
+                             backtrack(board, index + 1, row, col - 1, word);
             board[row][col] = word.charAt(index);
             return result;
         }
