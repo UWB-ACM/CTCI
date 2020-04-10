d9a5f660204fc1043f169f962b6783983c63c35b
Huy <49509023+huyn8@users.noreply.github.com>
2020-04-09 17:47:05 -0700

    Update 2020-02-Spring/0_arrays_matrix/buy_stock/driver.java
    Co-Authored-By: Lizzy Kate <lizzy.presland@gmail.com>

--- 2020-02-Spring/0_arrays_matrix/buy_stock/driver.java
+++ 2020-02-Spring/0_arrays_matrix/buy_stock/driver.java
@@ -7,11 +7,11 @@ public class driver{
         int [] arr4 = {7,1,5,3,6,4};
         int [] arr5 = {6,1,1,23,4,54,33,4,1,9,2,3,4,5,9,6,4,5,3,100};
 
-        Test("Max profit for arr1 is " + maxProfit(arr1), maxProfit(arr1) == 0);
-        Test("Max profit for arr2 is " + maxProfit(arr2), maxProfit(arr2) == 8);
-        Test("Max profit for arr3 is " + maxProfit(arr3), maxProfit(arr3) == 0);
-        Test("Max profit for arr4 is " + maxProfit(arr4), maxProfit(arr4) == 5);
-        Test("Max profit for arr5 is " + maxProfit(arr5), maxProfit(arr5) == 99);
+        Test("Max profit for arr1 is " + sol.maxProfit(arr1), sol.maxProfit(arr1) == 0);
+        Test("Max profit for arr2 is " + sol.maxProfit(arr2), sol.maxProfit(arr2) == 8);
+        Test("Max profit for arr3 is " + sol.maxProfit(arr3), sol.maxProfit(arr3) == 0);
+        Test("Max profit for arr4 is " + sol.maxProfit(arr4), sol.maxProfit(arr4) == 5);
+        Test("Max profit for arr5 is " + sol.maxProfit(arr5), sol.maxProfit(arr5) == 99);
 
     }
 
