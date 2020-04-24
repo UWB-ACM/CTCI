b105d1a9929b17e89d2ba1e3a4f49338ea12b178
Anish Prasad <33945547+anishprasad01@users.noreply.github.com>
2020-04-24 00:34:40 -0700

    Add comments to driver

--- 2020-02-Spring/1_strings/Add Strings/AddStrings.cpp
+++ 2020-02-Spring/1_strings/Add Strings/AddStrings.cpp
@@ -7,30 +7,38 @@ using namespace std;
 class Solution {
 public:
     string addStrings(string num1, string num2) {
+       // create integers that represent strings length
         int n1 = num1.size() - 1;
         int n2 = num2.size() - 1;
         int remain = 0;
         string ans;
-
+        // loops through while any condition is true
         while(n1 >= 0 || n2 >= 0 || remain) {
-            int d1;
-            if (n1 >= 0) {
+        int d1;
+        int d2;
+        // checks if values are left in num1
+            if (n1 >= 0){
+            // if so- turn d1 into int diff of string ASCII and '0'
                 d1 = num1[n1--] - '0';
-            } else {
+                } else {
                 d1 = 0;
             }
-            int d2;
-            //int d1 = (n1 >= 0) ? num1[n1--] - '0' : 0;
-            if (n2 >= 0) {
-                d2 = num2[n2--] - '0';
-            } else {
-                d2 = 0;
+            // Ternary equivalent
+            // int d1 = (n1 >= 0) ? num1[n1--] - '0' : 0;
+            if (n2 >= 0){
+                int d2 = num2[n2--] - '0';
+                } else {
+                int d2 = 0;
             }
-            //int d2 = (n2 >= 0) ? num2[n2--] - '0' : 0;
+            // Ternary equivalent
+            // int d2 = (n2 >= 0) ? num2[n2--] - '0' : 0;
+            
             int sum = d1 + d2 + remain;
+            // remain is 0 in integer division if value is less than 10
             remain = sum/10;
-            ans += '0' + (sum % 10);
+            ans += '0' + (sum % 10); 
         }
+        // reverse the string to form appropriate return value
         reverse(ans.begin(), ans.end());
         return ans;
     }
