126ca7a81aaf37dd37eddb25ecfdd1ac70c318cf
hollowsunsets <isabelgiang@gmail.com>
2020-04-29 12:55:10 -0700

    Add solution with manual memory management

--- 2020-02-Spring/2_linked_lists/reorder_list/cpp/Solution_Manual.cpp
+++ 2020-02-Spring/2_linked_lists/reorder_list/cpp/Solution_Manual.cpp
@@ -1,5 +1,12 @@
-#include <algorithm>
+// UWM ACM CTCI 
+// 05/01/2020 
+// Week 3: Linked Lists
+
+// Solution for LeetCode #143 Reorder List using manual memory management
+
+
 #include <iostream>
+#include <vector>
 
 struct ListNode {
     int val;
@@ -7,8 +14,36 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Helper method that inserts a new node at the end of the linked list
+void insert(ListNode* head, int element) {
+    ListNode* current = head;
+    while (current->next) {
+        current = current->next;
+    }
+    current->next = new ListNode(element);
 }
 
+// Helper method for creating lists for test cases
+ListNode* createBasicList(ListNode* head, std::vector<int> list) {
+    ListNode* front = new ListNode(0);
+    for (const int &value : list) {
+        insert(front, value);
+    }
+    return front->next;
+}
+
+// Helper method for printing the linked list
+void printList(ListNode* head) {
+    ListNode* current = head;
+    std::cout << "[";
+    if (current) std::cout << current->val;
+    while (current) {
+        std::cout <<  ", " << current->val; 
+    }
+    std::cout << "]" << std::endl;
+}
 
 void reorderList(ListNode *head) {
     // Find the middle element of the linked list
@@ -41,5 +76,5 @@ void reorderList(ListNode *head) {
 };
 
 int main() {
-
+    ListNode* curr;
 }
---
+++ 2020-02-Spring/2_linked_lists/reorder_list/cpp/Solution_SmartPtr.cpp
@@ -0,0 +1,17 @@
+// UWM ACM CTCI 
+// 05/01/2020 
+// Week 3: Linked Lists
+
+// Solution for LeetCode #143 Reorder List using shared pointers 
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    // shared_ptr<ListNode> next;
+};
+
+int main() {
+    return 0;
+
+}
