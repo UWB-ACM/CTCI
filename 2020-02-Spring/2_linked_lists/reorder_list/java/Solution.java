cabd48e59627ce628621f68845ef54425c89d715
hollowsunsets <isabelgiang@gmail.com>
2020-04-30 01:33:00 -0700

    Fix compilation errors for reorder_list

--- 2020-02-Spring/2_linked_lists/reorder_list/java/Solution.java
+++ 2020-02-Spring/2_linked_lists/reorder_list/java/Solution.java
@@ -6,6 +6,7 @@
 // Compile and run using: javac Solution.java ; java Solution.class
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
     public static class ListNode {
@@ -15,7 +16,7 @@ public class Solution {
     }
 
     // Helper method that inserts a new node at the end of the linked list
-    public ListNode insert(ListNode head, int element) {
+    public static ListNode insert(ListNode head, int element) {
         if (head == null) {
             return new ListNode(element);
         } else {
@@ -29,7 +30,7 @@ public class Solution {
     }
 
     // Helper method for creating lists for test cases
-    public ListNode makeBasicList(ArrayList<Integer> list) {
+    public static ListNode makeBasicList(ArrayList<Integer> list) {
         ListNode front = null;
         for (Integer value : list) {
             front = insert(front, value);
@@ -38,7 +39,7 @@ public class Solution {
     }
 
     // Helper method for printing the linked list
-    public void printList(ListNode head) {
+    public static void printList(ListNode head) {
         ListNode current = head;
         System.out.print("[");
         if (current != null) {
@@ -52,11 +53,14 @@ public class Solution {
         System.out.println("]");
     }
 
-    public void reorderList(ListNode head) {
+    public static void reorderList(ListNode head) {
+        // Edge case handling
+        if (head == null) return;
+        
         // Find the middle node of the linked list
         // 1->2->3->4->5->6, find 4
         ListNode slow = head, fast = head;
-        while (fast && fast.next) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -64,7 +68,7 @@ public class Solution {
         // Reverse the second half of the linked list
         // 1->2->3->4->5->6 ----> 1->2->3->4, 6->5->4
         ListNode previous = null, current = slow, temp;
-        while (current) {
+        while (current != null) {
             temp = current.next;
             current.next = previous;
             previous = current;
@@ -74,7 +78,7 @@ public class Solution {
         // Merge the two linked lists
         // 1->2->3->4, 6->5->4 ----> 1->6->2->5->3->4
         ListNode first = head, second = previous;
-        while (second.next) {
+        while (second.next != null) {
             temp = first.next;
             first.next = second;
             first = temp;
@@ -86,7 +90,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<>({1, 2, 3, 4});
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         ListNode testCase1 = makeBasicList(list1);
         System.out.print("Before: ");
         printList(testCase1);
@@ -95,7 +99,7 @@ public class Solution {
         System.out.print("After: ");
         printList(testCase1); // Expected: 1, 4, 2, 3
 
-        ArrayList<Integer> list2 = new ArrayList<>({1, 2, 3, 4, 5});
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
         ListNode testCase2 = makeBasicList(list2);
         System.out.print("Before: ");
         printList(testCase2);
