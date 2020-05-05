3897846b0bfd466fac21352841a8bd727a8568f5
etcadinfinitum <lizzy.presland@gmail.com>
2020-05-05 15:56:18 -0700

    Beautify solution code & add comments

--- 2020-02-Spring/3_trees/common_ancestor/solution.py
+++ 2020-02-Spring/3_trees/common_ancestor/solution.py
@@ -1,14 +1,22 @@
 def find_ancestor(root, p, q):
-    """
-    :type root: TreeNode
-    :type p: TreeNode
-    :type q: TreeNode
-    :rtype: TreeNode
-    """
+    # edge case -- root is null
     if not root: return None
-    if root.val == p.val or root.val == q.val: return root
+
+    # if we found one of the target nodes, return that node
+    if root.val == p.val or root.val == q.val:
+        return root
+
+    # if both nodes are in left-hand subtree, recurse 
+    # into that subtree to find the ancestor
     if root.val > p.val and root.val > q.val:
         return find_ancestor(root.left, p, q)
+
+    # if both nodes are in right-hand subtree, recurse 
+    # into that subtree to find the ancestor
     if root.val < p.val and root.val < q.val:
         return find_ancestor(root.right, p, q)
+
+    # otherwise, the nodes are distributed across the 
+    # two subtrees of the current node. this implies 
+    # that the current node is the LCA.
     return root
