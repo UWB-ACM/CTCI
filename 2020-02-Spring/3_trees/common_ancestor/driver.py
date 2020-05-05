feb49e4fcee1b51c9c70dc70ef65a7e691a8e828
etcadinfinitum <lizzy.presland@gmail.com>
2020-05-05 16:52:43 -0700

    Add additional tests

--- 2020-02-Spring/3_trees/common_ancestor/driver.py
+++ 2020-02-Spring/3_trees/common_ancestor/driver.py
@@ -3,6 +3,7 @@ from solution import *
 
 def main():
     assert(test_1())
+    assert(test_2())
 
 def test_1():
     t1 = make_test_1()
@@ -39,6 +40,38 @@ def make_test_1():
             'b', 'f', None, None, 't', 'z', 
             'a', 'c', 'e', 'h', None, None, None, None, 
             None, None, None, None, None, None, None, None]
+    return build_tree(vals)
+
+def test_2():
+    t = make_test_2()
+    p = t.left.left     # lower 3 node
+    q = t.left.right    # 4 node
+    lca = find_ancestor(t, p, q)
+    assert(lca.val == 3)
+    assert(lca.val == p.val)
+    assert(lca != p)
+    assert(lca != q)
+    q = find_node(t, 10)
+    lca = find_ancestor(t, p, q)
+    assert(lca == t)
+    assert(lca.val == 5)
+    return True
+
+def make_test_2():
+    '''
+    Tree:
+            5
+        3       9
+      3   4   7   11
+     1       6 8 10 12
+    0 2
+    '''
+    vals = [5,
+            3, 9,
+            3, 4, 7, 11,
+            1, None, None, None, 6, 8, 10, 12,
+            0, 2, None, None, None, None, None, None, None, None,
+            None, None, None, None]
     return build_tree(vals)
 
 if __name__=="__main__":
